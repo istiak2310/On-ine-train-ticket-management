fdb8f1d6e55db70e54a58ad5d19695da89a24ffa
istiak2310 <137644161+istiak2310@users.noreply.github.com>
2023-06-24 23:44:23 +0600

    Add files via upload

---
+++ splashcreen.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void splash_screen(void)
+{
+
+    printf("\n\n");
+    printf("**********************************************\n");
+    printf("*                                            *\n");
+    printf("*    Welcome to Railway Ticket Management    *\n");
+    printf("*                   System                   *\n");
+    printf("*                                            *\n");
+    printf("**********************************************\n");
+    printf("\t\t\tBy- Istiak Ahammed Rumi\n");
+     sleep(4);
+     system("CLS");
+
+}
